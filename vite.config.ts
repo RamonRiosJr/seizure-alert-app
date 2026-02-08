b4f3d737254106a28148ab306c61891594938c9b
Ramon Rios <info@coqui.cloud>
2026-02-08 03:13:27 -0700

    test: Add unit tests for useShake and useLocalStorage

--- vite.config.ts
+++ vite.config.ts
@@ -68,6 +68,8 @@ export default defineConfig(({ mode }) => {
       environment: 'jsdom',
       setupFiles: './src/test/setup.ts',
       css: true,
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      exclude: ['tests/**', 'node_modules/**'],
     }
   };
 });
